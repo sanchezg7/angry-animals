8fd2bab6aa28f40df085d9d6a49ff00fb59b0242
ayogsynergy <sanchezg.eng@gmail.com>
2024-05-11 13:52:52 -0400

    tinkering with sleep mode

---
+++ scenes/Bird2.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+@onready var label = $Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	label.text = "%s" % sleeping
+
+
+func _on_timer_timeout():
+	freeze = false
