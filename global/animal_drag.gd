32867d6be7d4dbc4ad75c6d26524758e9abbb568
ayogsynergy <sanchezg.eng@gmail.com>
2024-05-19 18:29:46 -0400

    encapsulated drag logic. godoto is pass by value in most cases

--- global/animal_drag.gd
+++ global/animal_drag.gd
@@ -1,13 +1,21 @@
-extends Node
+extends RigidBody2D
 
 # 60 px bounding box to gaurantee we fire in the right direction
 const DRAG_LIM_MAX: Vector2 = Vector2(0, 60) # top left point
 const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0) # bottom right point
 
-func hello():
-	print("hello")
+var _start: Vector2 = Vector2.ZERO
+# Where the drag starts when the user clicks
+var _drag_start: Vector2 = Vector2.ZERO
+func set_drag_start(value: Vector2) -> void:
+	_drag_start = value
 
-func drag_within_limits(_dragged_vector: Vector2, _start: Vector2) -> Vector2:
+# Track where the mouse has gone after recording drag start. Delta computed off of newest gmp
+var _dragged_vector: Vector2 = Vector2.ZERO
+func get_dragged_vector() -> Vector2:
+	return _dragged_vector
+	
+func get_position_dragged_within_limits(_dragged_vector: Vector2, _start: Vector2) -> Vector2:
 	# don't let x go outside the limits
 	_dragged_vector.x = clampf(
 		_dragged_vector.x,
@@ -18,3 +26,11 @@ func drag_within_limits(_dragged_vector: Vector2, _start: Vector2) -> Vector2:
 		DRAG_LIM_MIN.y, 
 		DRAG_LIM_MAX.y)
 	return _start + _dragged_vector
+	
+func calculate_dragged_vector(gmp: Vector2, _drag_start: Vector2) -> Vector2:
+	return gmp - _drag_start
+
+func get_dragged_position(gmp: Vector2, _start: Vector2) -> Vector2:
+	var raw_dragged_vector = calculate_dragged_vector(gmp, _drag_start)
+	_dragged_vector = get_position_dragged_within_limits(raw_dragged_vector, _start)
+	return _dragged_vector
--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -2,7 +2,6 @@ extends RigidBody2D
 
 @onready var animalDragEngine = preload("res://global/animal_drag.gd").new()
 
-
 @onready var debug_label = $DebugLabel
 
 enum ANIMAL_STATE { READY, DRAG, RELEASE }
@@ -16,20 +15,17 @@ const DRAG_LIM_MAX: Vector2 = Vector2(0, 60) # top left point
 const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0) # bottom right point
 
 var _start: Vector2 = Vector2.ZERO
-var _drag_start: Vector2 = Vector2.ZERO
-var _dragged_vector: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animalDragEngine.hello()
 	_start = position
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	update(delta)
 	# This will give a list of keys, since enums are a dict
+	var _dragged_vector = animalDragEngine.get_dragged_vector()
 	debug_label.text = "%s\n" % ANIMAL_STATE.keys()[_state]
 	debug_label.text += "%.1f,%.1f" % [_dragged_vector.x, _dragged_vector.y]
 
@@ -42,7 +38,7 @@ func set_release_state() -> void:
 	
 func set_drag_state() -> void:
 	_state = ANIMAL_STATE.DRAG
-	_drag_start = get_global_mouse_position()
+	animalDragEngine.set_drag_start(get_global_mouse_position())
 
 func has_user_released() -> bool:
 	if _state == ANIMAL_STATE.DRAG:
@@ -51,20 +47,13 @@ func has_user_released() -> bool:
 			return true
 	return false
 
-func get_dragged_vector(gmp: Vector2) -> Vector2:
-	return gmp - _drag_start
-
-func get_dragged_position() -> Vector2:
-	var gmp = get_global_mouse_position()
-	_dragged_vector = get_dragged_vector(gmp)
-	return animalDragEngine.drag_within_limits(_dragged_vector, _start)
-
 func update(delta: float) -> void:
+	var gmp = get_global_mouse_position()
 	match _state:
 		ANIMAL_STATE.DRAG:
 			if has_user_released() == true:
 				return
-			position = get_dragged_position()
+			position = animalDragEngine.get_dragged_position(gmp, _start)
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	AnimalManager.on_animal_died.emit()
