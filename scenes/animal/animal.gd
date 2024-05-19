28c23440a4e17bf381df3ab79c172a491d5b8602
ayogsynergy <sanchezg.eng@gmail.com>
2024-05-18 22:02:43 -0400

    update physics to drag on mouse drag and making sure pickable is set to true

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+@onready var debug_label = $DebugLabel
 
 enum ANIMAL_STATE { READY, DRAG, RELEASE }
 
@@ -15,15 +16,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	update(delta)
-	pass
+	# This will give a list of keys, since enums are a dict
+	debug_label.text = "%s" % ANIMAL_STATE.keys()[_state]
 
 func die() -> void: 
 	queue_free()
-
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	AnimalManager.on_animal_died.emit()
-	die()
 
 func set_release_state() -> void:
 	freeze = false
@@ -32,15 +29,15 @@ func set_release_state() -> void:
 func set_drag_state() -> void:
 	_state = ANIMAL_STATE.DRAG
 
-func detect_release() -> bool:
+func has_user_released() -> bool:
 	if _state == ANIMAL_STATE.DRAG:
-		if Input.is_action_just_released(ACTION_DRAG):
-			pass
+		if Input.is_action_just_released(ACTION_DRAG) == true:
+			set_release_state()
 			return true
 	return false
 
 func update_drag() -> void:
-	if detect_release() == true:
+	if has_user_released() == true:
 		return
 	var gmp = get_global_mouse_position()
 	position = gmp
@@ -50,6 +47,11 @@ func update(delta: float) -> void:
 		ANIMAL_STATE.DRAG:
 			update_drag()
 
-
-func _on_input_event(viewport, event, shape_idx):
-	pass # Replace with function body.
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	AnimalManager.on_animal_died.emit()
+	die()
+	
+func _on_input_event(viewport, event: InputEvent, shape_idx):
+	var isDragPressed = event.is_action_pressed(ACTION_DRAG)
+	if _state == ANIMAL_STATE.READY and isDragPressed:
+		set_drag_state()
