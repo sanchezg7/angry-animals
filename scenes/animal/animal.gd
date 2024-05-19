b519e99a49034f56406e515664c5758d5baa3515
ayogsynergy <sanchezg.eng@gmail.com>
2024-05-18 21:44:17 -0400

    init logic for dragging

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -1,6 +1,12 @@
 extends RigidBody2D
 
 
+enum ANIMAL_STATE { READY, DRAG, RELEASE }
+
+var _state: ANIMAL_STATE = ANIMAL_STATE.READY
+
+const ACTION_DRAG = "drag"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,6 +14,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	update(delta)
 	pass
 
 func die() -> void: 
@@ -17,3 +24,32 @@ func die() -> void:
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	AnimalManager.on_animal_died.emit()
 	die()
+
+func set_release_state() -> void:
+	freeze = false
+	_state = ANIMAL_STATE.RELEASE
+	
+func set_drag_state() -> void:
+	_state = ANIMAL_STATE.DRAG
+
+func detect_release() -> bool:
+	if _state == ANIMAL_STATE.DRAG:
+		if Input.is_action_just_released(ACTION_DRAG):
+			pass
+			return true
+	return false
+
+func update_drag() -> void:
+	if detect_release() == true:
+		return
+	var gmp = get_global_mouse_position()
+	position = gmp
+
+func update(delta: float) -> void:
+	match _state:
+		ANIMAL_STATE.DRAG:
+			update_drag()
+
+
+func _on_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
